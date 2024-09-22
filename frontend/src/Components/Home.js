f9b588ccc123d9cd9fc8eca1906d08570e9d53bd
Frenz Luiz <frenzluiz39@gmail.com>
2024-09-22 22:37:28 +0700

    Add Home.js

---
+++ frontend/src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="home">
+            <h2>Welcome to the Football Player Search App!</h2>
+            <p>Search, add, and manage your favorite football players.</p>
+            <p>Use the navigation above to get started.</p>
+        </div>
+    );
+};
+
+export default Home;
